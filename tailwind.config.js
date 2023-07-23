df664b7c02c9e4ba7094be78046c591ebe039296
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-07-23 16:42:01 -0400

    Configure prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./app/**/*.html.erb', './app/**/*.svelte'],
+  content: ["./app/**/*.html.erb", "./app/**/*.svelte"],
   theme: {
     extend: {},
   },
