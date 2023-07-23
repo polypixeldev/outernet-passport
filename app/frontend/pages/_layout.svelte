034019ff874cd3af84f4dff414f8ff001bace9fc
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-07-23 16:48:27 -0400

    Add header

---
+++ app/frontend/pages/_layout.svelte
@@ -0,0 +1,6 @@
+<header class="flex w-full items-center justify-start bg-blue-800 p-5">
+  <h1 class="font-cantarell text-5xl font-black text-white">
+    passport.outernet
+  </h1>
+</header>
+<slot />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./app/**/*.html.erb", "./app/**/*.svelte"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cantarell: ["Cantarell", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
